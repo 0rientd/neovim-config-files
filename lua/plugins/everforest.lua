ef9da446c3a3808dbde2647a7ec26c9f94db366b
Carlos Henrique da Silva Barros Jr. <carlosjunior@unimedrj.coop.br>
2025-12-11 18:53:16 -0300

    alterado tema utilizado

---
+++ lua/plugins/everforest.lua
@@ -0,0 +1,10 @@
+return {
+  "sainnhe/everforest",
+  lazy = false,
+  name = "everforest", 
+  priority = 1000,
+  config = function()
+    -- Theme
+    vim.cmd.colorscheme "everforest" 
+  end
+}
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "ruby_lsp",
+          "cssls",
+          "dockerls",
+          "html",
+          "jsonls",
+          "pylsp",
+          "tailwindcss",
+          "terraformls",
+          "yamlls"
+        },
+        handlers = {
+          function(server)
+            vim.lsp.config[server].setup({})
+          end,
+
+          lua_ls = function()
+            vim.lsp.config.lua_ls.setup({
+              settings = {
+                Lua = {
+                  diagnostics = { globals = { "vim" } }
+                }
+              }
+            })
+          end,
+        }
+      })
+    end
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
