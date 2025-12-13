410454e1db236ccd6630cd2a36e48c64338de68c
Carlos 0rientd Henrique <carlos.henrique@0rientd.dev.br>
2025-12-13 18:12:21 -0300

    adicionado keymap para comandos do github

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,4 +5,6 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
-
+vim.keymap.set('n', '<leader>gd', ':! git diff --graph<CR>', {})
+vim.keymap.set('n', '<leader>gs', ':! git status<CR>', {})
+vim.keymap.set('n', '<leader>gp', ':! git pull<CR>', {})
