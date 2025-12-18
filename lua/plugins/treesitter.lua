f10bce139f1a42f384ca9a4a0b3f6d1e0bafaf68
Carlos Henrique da Silva Barros Jr. <carlosjunior@unimedrj.coop.br>
2025-12-18 19:15:32 -0300

    pequena modificacao no plugin do treesitter e adicionado plugin none-ls

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  branch = 'master', 
-  lazy = false, 
-  build = ":TSUpdate",
-  config = function()
-    -- Treesitter
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "javascript", "ruby", "html", "vim", "markdown", "query", "css", "json", "typescript", "yaml", "tsx", "gitignore", "dockerfile", "diff", "c", "bash", },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		-- Treesitter
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
