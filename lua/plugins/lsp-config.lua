84fbf3400eb5bdf77903e4f827c706e93accd3ec
Carlos Henrique <carlos.henrique@0rientd.dev.br>
2025-12-18 17:40:37 +0000

    ajustado lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,9 +14,11 @@ return {
   -- -------------------------
   {
     "williamboman/mason-lspconfig.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
+          "lua_ls",
           "ts_ls",
           "cssls",
           "dockerls",
@@ -27,42 +29,52 @@ return {
           "terraformls",
           "yamlls"
         },
-
-        handlers = {
-          -- Handler padrão (exceto Ruby)
-          function(server)
-            vim.lsp.config(server, {})
-          end,
-
-          -- Lua com config especial
-          lua_ls = function()
-            vim.lsp.config("lua_ls", {
-              settings = {
-                Lua = {
-                  diagnostics = {
-                    globals = { "vim" }
-                  }
-                }
-              }
-            })
-          end,
-        }
+        automatic_installation = true,
       })
     end
   },
 
   -- -------------------------
-  -- LSPConfig manual (Ruby)
+  -- LSPConfig
   -- -------------------------
   {
     "neovim/nvim-lspconfig",
     config = function()
-      -- Keymaps globais 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      local lspconfig = require('lspconfig')
+      local on_attach = function(client, bufnr)
+        local bufmap = function(keys, func)
+          vim.keymap.set('n', keys, func, { buffer = bufnr })
+        end
 
-      vim.lsp.config("ruby_lsp", {
+        bufmap('K', vim.lsp.buf.hover)
+        bufmap('gD', vim.lsp.buf.declaration)
+        bufmap('gd', vim.lsp.buf.definition)
+        bufmap('gi', vim.lsp.buf.implementation)
+        bufmap('gr', vim.lsp.buf.references)
+        bufmap('<leader>rn', vim.lsp.buf.rename)
+        bufmap('<leader>ca', vim.lsp.buf.code_action)
+      end
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('user-lsp-attach', { clear = true }),
+        callback = function(event)
+          on_attach(vim.lsp.get_client_by_id(event.data.client_id), event.buf)
+        end,
+      })
+
+      -- Custom setup for lua_ls
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' },
+            },
+          },
+        },
+      })
+
+      -- Manual LSP config
+      lspconfig.ruby_lsp.setup({
         cmd = { "bundle", "exec", "ruby-lsp" },
         filetypes = { "ruby" },
         root_dir = vim.fs.root(0, {
