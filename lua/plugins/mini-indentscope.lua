6d92d3da18986e72101e65236f651bba84a37f84
Carlos Henrique <carlosbarros@unimedrj.coop.br>
2026-06-02 12:32:50 -0300

    atualizado neovim para incluir plugin de visualização de identação

---
+++ lua/plugins/mini-indentscope.lua
@@ -0,0 +1,32 @@
+return {
+  "echasnovski/mini.indentscope",
+  version = false,
+  event = { "BufReadPre", "BufNewFile" },
+
+  opts = {
+    -- symbol = "│",
+    symbol = "╎",
+    options = { try_as_border = true },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "alpha",
+        "dashboard",
+        "fzf",
+        "help",
+        "lazy",
+        "lazyterm",
+        "mason",
+        "neo-tree",
+        "notify",
+        "toggleterm",
+        "Trouble",
+        "trouble",
+      },
+      callback = function()
+        vim.b.miniindentscope_disable = true
+      end,
+    })
+  end,
+}
